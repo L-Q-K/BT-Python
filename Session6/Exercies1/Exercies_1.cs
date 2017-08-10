5339ee1b395786d30ec432d5adb1b5f64452669e
L-Q-K <kykise@gmail.com>
2017-08-10 12:01:22 +0700

    Session 6 homework commited

---
+++ Session6/Exercies1/Exercies_1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercies1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write("What is your height (cm): ");
+			float height = Convert.ToInt32(Console.ReadLine ());
+			height = height / 100;
+
+			Console.Write ("What is your weight (kg): ");
+			int weight = Convert.ToInt32(Console.ReadLine ());
+
+			float BMI = (float) weight / (height * height);
+			Console.WriteLine (BMI);
+
+			if (BMI < 16)
+			{
+				Console.WriteLine("Severely underweight");
+			}
+			else if (BMI < 18.5)
+			{
+				Console.WriteLine("Underweight");
+			}
+			else if (BMI < 25 )
+			{
+				Console.WriteLine(" Normal ");
+			}
+			else if (BMI < 30)
+			{
+				Console.WriteLine("Overweight");
+			}
+			else
+			{
+				Console.WriteLine("Obese");
+			}
+		}
+	}
+}
---
+++ Session6/Exercies2/Exercies_2_and_3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercies2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Ex 2: too ez with C#
+			Console.Write ("Hello ");
+			Console.Write (", my name is ");
+			Console.Write ("B-max ");
+
+			Console.WriteLine ("");
+			// Ex 3 : Programs that scale
+			Console.Write ("Input Colums: ");
+			int colums = Convert.ToInt32(Console.ReadLine ()); 
+
+			Console.Write ("Input Rows: ");
+			int rows = Convert.ToInt32(Console.ReadLine ());
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < colums; j++) 
+				{
+					if (i % 2 == 0) 
+					{
+						Console.Write ("x*");
+					} 
+					else 
+					{
+						Console.Write ("*x");
+					}
+				}
+				Console.WriteLine ();
+			}
+		}
+	}
+}
